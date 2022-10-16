0616a0c2a81a1c9f472fdb2beba11967831ae266
brazadrian <gilmaradrian127@gmail.com>
2022-10-16 19:42:35 -0300

    comentarios didividindo as consultas dql

--- Scripts - SQL/DQL_Saude_menos.sql
+++ Scripts - SQL/DQL_Saude_menos.sql
@@ -12,6 +12,8 @@ SELECT pac.nome "Nome", pac.cpf "CPF", pac.num_prontuario "Nº Prontuário", pac
 	INNER JOIN Consultas AS cst ON pac.num_prontuario = cst.Pacientes_num_prontuario
 	WHERE cst.data_consulta BETWEEN '2020-01-01 00:01' AND '2022-10-12'
 		ORDER BY cst.data_consulta DESC;
+
+-- -----------------------------------------------------
         
 /*
 2 (SEGUNDA) CONSULTA
@@ -26,6 +28,8 @@ SELECT med.cpf, med.crm, med.nome, med.especialidade, tel.num_telefone
     WHERE med.crm like '%PE'
 		ORDER BY med.nome ASC;
 
+-- -----------------------------------------------------
+
 /*
 3 (TERCEIRA) CONSULTA
 Listar os pacientes que são do bairro de Santo Antônio e possuem ao menos 1 exame realizado,
@@ -37,6 +41,7 @@ SELECT pac.num_prontuario "Prontuário", pac.nome "Nome", pac.cpf "CPF", pac.sex
 	INNER JOIN Exames AS exa ON exa.Pacientes_num_prontuario = pac.num_prontuario
 		WHERE edr.bairro = 'Santo Antônio';
         
+-- -----------------------------------------------------
 
 /*
 4 (QUARTA) CONSULTA (incompleta)
@@ -51,11 +56,14 @@ SELECT pac.num_prontuario "Nº Prontuário", pac.cpf "CPF", pac.nome "Nome", pac
         WHERE cst.data_consulta IS NOT NULL
         GROUP BY cst.Pacientes_num_prontuario, pac.num_prontuario, pac.cpf, pac.nome, pac.sexo, cst.data_consulta
         ORDER BY pac.nome;
+        
 /*
 SELECT COUNT(cst.Pacientes_num_prontuario), cst.data_consulta
 	FROM Consultas AS cst
 		INNER JOIN Pacientes AS pac;
 */
+
+-- -----------------------------------------------------
 
 /*
 5 (QUINTA) CONSULTA
@@ -70,6 +78,8 @@ SELECT med.cpf "CPF", med.crm "CRM", med.nome "Nome", med.salario "Salário", e.
         GROUP BY med.cpf, med.crm, med.nome, med.salario, e.bairro
 		HAVING salario < (SELECT AVG(salario) FROM Medicos)
 			ORDER BY med.nome;
+
+-- -----------------------------------------------------
 
 /*
 6 (SEXTA) CONSULTA
@@ -87,6 +97,8 @@ SELECT med.cpf "CPF", med.crm "CRM", med.nome "Nome", med.especialidade "Especia
 		HAVING `salario` > (SELECT AVG(salario) FROM Medicos)
             ORDER BY med.nome;
 
+-- -----------------------------------------------------
+
 /*
 7 (SÉTIMA) CONSULTA
 Listar nome de pacientes e a quantidade de exames realizadas por eles,
@@ -103,6 +115,7 @@ SELECT pac.nome "Nome", COUNT(exa.Pacientes_num_prontuario) "Exames", pac.sexo, 
 			GROUP BY(pac.num_prontuario)
             ORDER BY edr.bairro DESC, pac.sexo;
 
+-- -----------------------------------------------------
 
 /*
 8 (OITAVA) CONSULTA
@@ -123,6 +136,8 @@ ON cst.Medicos_cpf = med.cpf
 WHERE cst.data_consulta IS NOT NULL
 GROUP BY(Dia)
 ORDER BY cst.data_consulta;
+
+-- -----------------------------------------------------
 
 /*
 9 (NONA) CONSULTA
@@ -165,6 +180,8 @@ Tabela virtual criada para auxílio nesta consulta (9ª)
 
 */
 
+-- -----------------------------------------------------
+
 /*
 10 (DÉCIMA) CONSULTA
 Listar a quantidade de casos de câncer e separar por sexo acometidos
@@ -172,6 +189,7 @@ trazendo as colunas sexo e nome_patologia
 ordenar por ordem decrescente. (join)
 */
 
+-- -----------------------------------------------------
 
 /*
 11 (DÉCIMA PRIMEIRA) CONSULTA
@@ -180,6 +198,7 @@ trazendo as colunas medicos e nome_patologia
 ordenar por ordem crescente. (join)
 */
 
+-- -----------------------------------------------------
 
 /*
 12 (DÉCIMA SEGUNDA) CONSULTA
@@ -188,6 +207,8 @@ trazer as colunas nome (médico), nome (hospital)
 ordene por Hospital. (2 join)
 */
 
+-- -----------------------------------------------------
+
 /*
 13 (DÉCIMA TERCEIRA) CONSULTA
 Informe a soma de solicitações realizadas por dia
@@ -195,11 +216,15 @@ trazendo as colunas data (solicitação), nome do hospital e nome laboratório
 ordene por hospital. (2 join)
 */
 
+-- -----------------------------------------------------
+
 /*
 14 (DÉCIMA QUARTA) CONSULTA
 Listar as especialidades contabilizando o número total de médicos por especialidade do hospital “xxxx”
 trazendo as colunas total de médicos e especialidade. (join)
 */
+
+-- -----------------------------------------------------
 
 /*
 15 (DÉCIMA QUINTA) CONSULTA
@@ -214,6 +239,7 @@ SELECT p.nome, t.num_telefone, e.cep FROM pacientes as p
 		WHERE t.ddd = ‘11’
 ORDER BY p.nome;
 
+-- -----------------------------------------------------
 
 /*
 16 (DÉCIMA SEXTA) CONSULTA
@@ -225,12 +251,13 @@ SELECT p.num_prontuario, p.cpf, p.nome, p.nome_social, p.sexo FROM paciente as p
 INNER JOIN Exames as ex ON ex.Pacientes_num_prontuario = p.num_prontuario
 	WHERE ex.id_exame is null AND p.nome LIKE %Silva%;
 
+-- -----------------------------------------------------
 
 /*
 17 (DÉCIMA SÉTIMA) CONSULTA
 */
 
-
+-- -----------------------------------------------------
 
 /*
 18 (DÉCIMA OITAVA) CONSULTA
@@ -242,6 +269,7 @@ SELECT nome, data_nascimento, num_prontuario FROM Pacientes
 	WHERE data_nascimento BETWEEN '1940-01-01' AND '1980-01-01'
 	ORDER BY data_nascimento;
 
+-- -----------------------------------------------------
 
 /*
 19 (DÉCIMA NONA) CONSULTA
@@ -254,6 +282,7 @@ LEFT JOIN hospitais AS h ON h.Enderecos_id_endereco = e.id_endereco
 LEFT JOIN medicos AS m ON m.Enderecos_id_endereco = e.id_endereco
 WHERE e.bairro = 'Boa Vista';
 
+-- -----------------------------------------------------
 
 /*
 20 (VIGÉSIMA) CONSULTA
@@ -265,17 +294,4 @@ FROM telefones AS t
 INNER JOIN pacientes AS p ON p.Telefones_ddd = t.ddd
 ORDER BY p.nome;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- -----------------------------------------------------
